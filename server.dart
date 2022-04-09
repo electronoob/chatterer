e3dc402ba7da5b7f4aed32d435c4ef4ed1f1b5d3
electronoob <github@electronoob.com>
2022-04-09 21:55:08 +0000

    ew tabs

--- server.dart
+++ server.dart
@@ -2,14 +2,14 @@ import 'dart:io';
 import 'dart:convert';
 
 void main() {
-ServerSocket.bind('0.0.0.0', 6667)
-  .then((serverSocket) {
-    serverSocket.listen((socket) {
-      print("We have a new connection");
-print(socket);
-      socket.listen(onData);
-    });
-  });
+	ServerSocket.bind('0.0.0.0', 6667)
+		.then((serverSocket) {
+			serverSocket.listen((socket) {
+				print("We have a new connection");
+				print(socket);
+				socket.listen(onData);
+			});
+		});
 }
 
 void onData(event) {
