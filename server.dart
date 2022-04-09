8fa66f78a8f69672a8d5440c027723f218051f86
electronoob <github@electronoob.com>
2022-04-09 23:53:19 +0000

    parsed a bit more of the incomming data and write nonsense back to connecting socket

--- server.dart
+++ server.dart
@@ -8,10 +8,31 @@ void main() {
 				print("We have a new connection");
 				print(socket);
 				socket.listen(onData);
+				socket.writeln("NOTICE AUTH :*** something something lol lol");
+				socket.writeln(":Singapore.SG.AS.Undernet.Org 001 user_ :welcome to irc, user_");
 			});
 		});
 }
 
 void onData(event) {
-	print(utf8.decode(event));
+	String data = utf8.decode(event);
+	LineSplitter ls = new LineSplitter();
+	List<String> lines = ls.convert(data);
+	for (var line in lines) {
+		print("-");
+		print(line);
+		//split line by spaces
+		List<String> toks = line.split(' ');
+		print(toks);
+		if (toks[0] == "NICK") {
+			print("NICK REQUEST RECEIVED");
+			print(toks.length);
+			if(toks.length == 2) { 
+				print(toks[1]);
+			} else {
+				print("incorrect paramaters provided.");
+			}
+		}
+
+	}
 }
